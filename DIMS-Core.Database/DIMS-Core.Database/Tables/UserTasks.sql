d15ae03da6d1989eac46c6e808ddfa5929cd76cf
Dmitry <dmitri.bohdan@gmai.com>
2021-02-19 21:37:44 +0300

    changed string length in Description field to 255. Marked Description as null

--- DIMS-Core.Database/DIMS-Core.Database/Tables/UserTasks.sql
+++ DIMS-Core.Database/DIMS-Core.Database/Tables/UserTasks.sql
@@ -1,11 +1,11 @@
 create table [dbo].[UserTasks]
 (
-[UserTaskId] INT NOT NULL PRIMARY KEY IDENTITY,
-[TaskId] INT NOT NULL,
-[UserId] INT NOT NULL,
-[StateId] INT NOT NULL
+	[UserTaskId] INT NOT NULL PRIMARY KEY IDENTITY,
+	[TaskId] INT NOT NULL,
+	[UserId] INT NOT NULL,
+	[StateId] INT NOT NULL
 
-FOREIGN KEY (TaskId) REFERENCES Tasks(TaskId),
-FOREIGN KEY (UserId) REFERENCES UserProfiles(UserId),
-FOREIGN KEY (StateId) REFERENCES TaskStates(StateId),
+	FOREIGN KEY (TaskId) REFERENCES Tasks(TaskId),
+	FOREIGN KEY (UserId) REFERENCES UserProfiles(UserId),
+	FOREIGN KEY (StateId) REFERENCES TaskStates(StateId),
 )
