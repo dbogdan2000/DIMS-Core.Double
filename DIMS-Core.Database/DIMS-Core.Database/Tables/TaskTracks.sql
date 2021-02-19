c36fe734880eb817fcefb9ce735127e24c5b3a66
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-19 18:35:22 +0300

    Add TaskTracks table

---
+++ DIMS-Core.Database/DIMS-Core.Database/Tables/TaskTracks.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TaskTracks]
+(
+    [TaskTrackId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserTaskId] INT NOT NULL, 
+    [TrackDate] DATETIME NOT NULL, 
+    [TrackNote] NVARCHAR(250) NULL,
+
+    FOREIGN KEY (UserTaskId) REFERENCES UserTasks(UserTaskId)
+)
