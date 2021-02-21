a46ec08414d1e711742b0f26ed0ffcffa25fce88
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-02-21 13:30:41 +0300

    Add UserTracks view

---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserTracks.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vUserTracks]
+	AS SELECT 
+		UserProfiles.UserId,
+		Tasks.TaskId,
+		TaskTracks.TaskTrackId,
+		Tasks.[Name] as TaskName,
+		TaskTracks.TrackNote,
+		TaskTracks.TrackDate
+		FROM UserProfiles
+		INNER JOIN UserTasks on UserProfiles.UserId = UserTasks.UserId
+		INNER JOIN TaskTracks on UserTasks.TaskId = TaskTracks.UserTaskId
+		INNER JOIN Tasks on UserTasks.TaskId = Tasks.TaskId
